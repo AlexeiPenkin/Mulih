f8f5496e6b9b71a311a7061bbaa3ad9e27791b45
Alexei Penkin <alexei_penkin@mail.ru>
2023-04-05 14:58:19 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Picture Popup
+function openPopup() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
